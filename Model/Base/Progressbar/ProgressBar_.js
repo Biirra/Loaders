22334bae6e3d1f269f7ed845b1ce31955c0486e4
Jan Willem Huising <jw.huising@gmail.com>
2019-10-08 20:05:38 +0200

    Create ProgressBar_.js

---
+++ Model/Base/Progressbar/ProgressBar_.js
@@ -0,0 +1,12 @@
+/*
+If player presses button.
+    add work to progressbarr
+
+while there is work
+    do work
+
+Work()
+    if(currentProgres + work < maxProgres)
+        add speed to current progress;
+    
+*/
